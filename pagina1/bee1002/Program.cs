72e923ee04f2b089894780de9315ee5b108f5589
felipefaraujo <devfelipefaraujo@gmail.com>
2023-07-15 09:51:32 -0300

    Página 1 - Iniciante

---
+++ pagina1/bee1002/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double raio = double.Parse(Console.ReadLine());
+
+        double pi = 3.14159;
+        double area = pi * Math.Pow(raio, 2);
+
+        Console.WriteLine("A=" + area.ToString("F4"));
+    }
+}
